d267cec428655925a008941e1ced081fd244f289
Tomer Horowitz <tomerh@realcommerce.co.il>
2025-02-02 22:52:27 +0200

    feat: add configuration files for Yarn and XO, update TypeScript settings, and refactor extension code

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,15 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "jira-branches" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('jira-branches.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Jira Branches!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,9 +1,7 @@
-import * as assert from 'assert';
+/* eslint-disable @typescript-eslint/no-floating-promises */
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+import * as assert from 'node:assert';
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
