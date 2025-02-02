068bc36b2267ec741582db24c3a06ace855c0869
Tomer Horowitz <tomerh@realcommerce.co.il>
2025-02-02 23:08:21 +0200

    feat: update helloWorld command to use async/await and show quick pick options

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/no-floating-promises */
+
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "jira-branches" is now active!');
 
-	const disposable = vscode.commands.registerCommand('jira-branches.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from Jira Branches!');
+	const disposable = vscode.commands.registerCommand('jira-branches.helloWorld', async () => {
+		const result = await vscode.window.showQuickPick(['one', 'two', 'three'], {title: 'Pick one'});
+		console.log(result);
 	});
 
 	context.subscriptions.push(disposable);
