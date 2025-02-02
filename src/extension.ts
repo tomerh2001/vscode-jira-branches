b59fca84b722925b9cc78552ac67c6f1dc7efdee
Tomer Horowitz <tomerh@realcommerce.co.il>
2025-02-02 23:21:48 +0200

    chore: update command to setup Jira and add configuration options for Jira URL and token

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,24 @@
 
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "jira-branches" is now active!');
 
-	const disposable = vscode.commands.registerCommand('jira-branches.helloWorld', async () => {
-		const result = await vscode.window.showQuickPick(['one', 'two', 'three'], {title: 'Pick one'});
-		console.log(result);
+	vscode.register;
+	const disposable = vscode.commands.registerCommand('jira-branches.setup', async () => {
+		const jiraUrl = await vscode.window.showInputBox({
+			placeHolder: 'Enter your Jira URL',
+			prompt: 'Enter your Jira URL',
+		});
+
+		const jiraToken = await vscode.window.showInputBox({
+			placeHolder: 'Enter your Jira API token',
+			prompt: 'Enter your Jira API token',
+		});
+
+		await vscode.workspace.getConfiguration().update('jira-branches.jiraUrl', jiraUrl, vscode.ConfigurationTarget.Global);
+		await vscode.workspace.getConfiguration().update('jira-branches.jiraToken', jiraToken, vscode.ConfigurationTarget.Global);
+		await vscode.window.showInformationMessage('Jira URL and token saved successfully!');
 	});
 
 	context.subscriptions.push(disposable);
